0cd0c107fb1fc1ec4db7e27636a9ba3cc70bfeb7
Burak Kaygusuz <kaygusuzburak@gmail.com>
2024-09-24 11:16:11 +0300

    Use flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import cypress from 'eslint-plugin-cypress';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['**/node_modules/'],
+  },
+  ...compat.extends('eslint:recommended', 'plugin:cypress/recommended'),
+  {
+    plugins: {
+      cypress,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+        ...cypress.environments.globals.globals,
+      },
+
+      ecmaVersion: 'latest',
+    },
+
+    rules: {},
+  },
+];
